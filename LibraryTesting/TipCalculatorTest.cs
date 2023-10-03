f6534607740a0dcc302e5dd4878e632d3ff57eaf
Dennis Padilla <d_padilla.y@hotmail.com>
2023-10-02 22:25:19 -0400

    Created 3 tests for method SplitAmount

--- LibraryTesting/TipCalculatorTest.cs
+++ LibraryTesting/TipCalculatorTest.cs
@@ -1,10 +1,27 @@
+using System.Diagnostics;
+
 namespace TipLibrary.Tests;
 
 [TestClass]
-public class UnitTest1
+public class TipCalculatorTest
 {
-    [TestMethod]
-    public void TestMethod1()
+      [TestMethod]
+    public void Test_SplitAmount_ReturnInteger()
     {
+        decimal result = TipCalculator.SplitAmount(100m, 4);
+        Assert.AreEqual(25m, result);
+    }
+
+    [TestMethod]
+    public void Test_SplitAmount_ReturnDecimal()
+    {
+        decimal result = TipCalculator.SplitAmount(100m, 3);
+        Assert.AreEqual(33.33m, result);
+    }
+
+    [TestMethod]
+    public void Test_SplitAmount_By_ZeroPeople()
+    {
+         Assert.ThrowsException<DivideByZeroException>(() => {TipCalculator.SplitAmount(100,0); });
     }
 }
