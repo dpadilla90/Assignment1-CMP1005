8a1cb7da0acd49449855fe68b1d0baac9932e094
Dennis Padilla <d_padilla.y@hotmail.com>
2023-10-02 22:24:54 -0400

    Created Method to split amount

--- Library/TipCalculator.cs
+++ Library/TipCalculator.cs
@@ -1,6 +1,14 @@
 ﻿namespace TipLibrary;
 
-public class TipCalculator
+public static class TipCalculator
 {
+    public static decimal SplitAmount(decimal amount, int numberOfPeople)
+    {
+         if (numberOfPeople == 0)
+         {
+            throw new DivideByZeroException("Number of people should not be zero.");
+         } 
+        return Math.Round(amount / numberOfPeople, 2);
+    }
 
 }
