d0634507e4877399e0e3ac19a687b0d0722de135
Dennis Padilla <d_padilla.y@hotmail.com>
2023-10-03 21:32:40 -0400

    Added three tests for the Calculate Tip method

--- LibraryTesting/TipCalculatorTest.cs
+++ LibraryTesting/TipCalculatorTest.cs
@@ -24,4 +24,64 @@ public class TipCalculatorTest
     {
          Assert.ThrowsException<DivideByZeroException>(() => {TipCalculator.SplitAmount(100,0); });
     }
+
+   
+
+     [TestMethod]
+    public void Test_CalculateTip_ThreePerson_ReturnCorrectTip()
+    {
+        var mealCosts = new Dictionary<string, decimal>
+        {
+            { "Juan", 50.0m },
+            { "Janeth", 30.0m },
+            { "Miriam", 20.0m }
+        };
+
+        var tips = new Dictionary<string, decimal>
+        {
+            { "Juan", 7.5m },
+            { "Janeth", 4.5m },
+            { "Miriam", 3.0m }
+        };
+
+        var result = TipCalculator.CalculateTip(mealCosts, 0.15f);
+
+        CollectionAssert.AreEqual(tips, result);
+    }
+
+    [TestMethod]
+    public void Test_CalculateTip_ThreePerson_NoTips()
+    {
+        var mealCosts = new Dictionary<string, decimal>
+        {
+            { "Juan", 50.0m },
+            { "Janeth", 30.0m },
+            { "Miriam", 20.0m }
+        };
+
+        var tips = new Dictionary<string, decimal>
+        {
+            { "Juan", 0.0m },
+            { "Janeth", 0.0m },
+            { "Miriam", 0.0m }
+        };
+
+        var result = TipCalculator.CalculateTip(mealCosts, 0.0f);
+
+        CollectionAssert.AreEqual(tips, result);
+    }
+
+     [TestMethod]
+    public void Test_CalculateTip_ThreePerson_IncorrectPercentage()
+    {
+         var mealCosts = new Dictionary<string, decimal>
+        {
+            { "Juan", 50.0m },
+            { "Janeth", 30.0m },
+            { "Miriam", 20.0m }
+        };
+         Assert.ThrowsException<ArgumentOutOfRangeException>(() => {TipCalculator.CalculateTip(mealCosts,1.5f); });
+    }
+
+    
 }
