8425c64b2a293ae640c2c1b33488dc95740f27e5
Dennis Padilla <d_padilla.y@hotmail.com>
2023-10-03 21:30:30 -0400

    Created Method to calculate TipAmount

--- Library/TipCalculator.cs
+++ Library/TipCalculator.cs
@@ -11,4 +11,33 @@ public static class TipCalculator
         return Math.Round(amount / numberOfPeople, 2);
     }
 
+    public static Dictionary<string, decimal> CalculateTip(Dictionary<string, decimal> mealCosts, float tipPercentage)
+    {
+     
+         //Check if percentage is between 0 an 1
+        if (tipPercentage < 0 || tipPercentage > 1)
+        {
+            throw new ArgumentOutOfRangeException("Percentage must be a number between 0 and 1");
+        }
+
+        // Calculate the total cost of the meals
+        decimal totalCost = 0;
+        foreach (var cost in mealCosts.Values)
+        {
+            totalCost += cost;
+        }
+
+        // Calculate the total tip amount
+        decimal totalTip = totalCost * (decimal)tipPercentage;
+
+        // Calculate the tip for each person based on the weighted average 
+        Dictionary<string, decimal> tips = new Dictionary<string, decimal>();
+        foreach (var entry in mealCosts)
+        {
+            decimal individualTip = (entry.Value / totalCost) * totalTip;
+            tips[entry.Key] = Math.Round(individualTip, 2);
+        }
+
+        return tips;
+    }
 }
